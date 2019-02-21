fd4d648329f74d4d98868c01b4c7ef3cbea45d3f
Komalpreet_Gill <komalgillc3@gmail.com>
2019-02-21 14:52:24 -0500

    commit

--- C0725450_week07/Program.cs
+++ C0725450_week07/Program.cs
@@ -10,6 +10,31 @@ namespace C0725450_week07
     {
         static void Main(string[] args)
         {
+            var a = new PlayWithRecursion();
+            Console.WriteLine(PlayWithRecursion.GetFactorial(4));
         }
     }
+    class PlayWithRecursion
+    {
+        public void Add(int a, int b)
+        {
+            if (a > 10)
+            {
+                return;
+            }
+
+            Add(++a, --b);
+        }
+
+        public static long GetFactorial(int number)
+        {
+            Console.WriteLine(number);
+            if(number == 0) { return 1; }
+
+            return number * GetFactorial(number - 1);
+        }
+        
+    }
+    // Recursion is a method calls itself
+    //Every time call a method it will create a frame in the stack
 }
